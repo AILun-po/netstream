b9c7a7a076a1c9a498fe94c56f9dfd5b8a81b513
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2015-09-17 13:13:14 +0200

    Added more tests.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,106 +1,193 @@
 #!/bin/sh
 
 run_test () {
-	echo -n "Running test $1 ($2)... "
-	../netstream -c ${1}.conf > /dev/null 2>&1
-	NSPID=$!
+	if [ x$3 != xq ]
+	then
+		echo -n "Running test $1 ($2)... "
+	fi
+	if [ x$3 != xb ]
+	then
+		../netstream -c ${1}.conf > /dev/null 2>&1
+	#	../netstream -c ${1}.conf -v7 
+		RES=$?
+	else
+		../netstream -c ${1}.conf > /dev/null 2>&1 & >/dev/null 2>&1
+	#	../netstream -c ${1}.conf -v7 & >/dev/null 2>&1 
+		NSPID=$!
+	fi
 }
 check_result () {
 	diff ${1}.in ${2}.out > /dev/null
 	if [ $? = 0 ]
 	then
-		echo "OK"
+		RES=0
+		return;
 	else
-		echo "Failed"
+		RES=1
+		return;
 	fi
 }
+print_result() {
+	if [ $RES -eq 0 ]
+	then
+		if [ x$1 = x ]
+		then
+			echo "OK"
+		fi
+	else
+		echo "Failed"
+		FAIL=1
+	fi
+}
+qkill () {
+	kill $1 >/dev/null 2>&1
+}
 
+RES=1	# Result of a test
+FAIL=0	# Any of tests failed
+
+# Test 1
 rm -f 1.out
 run_test 1 "file -> file"
+print_result q
 check_result "a" 1
+print_result
 
+#Test 2
 rm -f 2.out
 nc -lp 3000 > 2.out & > /dev/null 2>&1
 NCPID=$!
 sleep 1
 run_test 2 "file -> TCP"
+print_result q
 sleep 1
 check_result "a" 2
-kill $NCPID >/dev/null 2>&1
+print_result
+qkill $NCPID
 
-UDP=0
-while [ $UDP -lt 8192 ]
+#Test 3
+for i in `seq 10`
 do
 	rm -f 3.out
 	nc -lup 3000 > 3.out & > /dev/null 2>&1
 	NCPID=$!
-	run_test 3 "file -> UDP"
+	if [ $i -eq 1 ]
+	then
+		run_test 3 "file -> UDP"
+	else 
+		run_test 3 "file -> UDP" q
+	fi
+	print_result q
 	sleep 1
-	UDP=`cat 3.out | wc -c`
-	echo -n $UDP "-> "
+	#UDP=`cat 3.out | wc -c`
+	#echo -n $UDP "-> "
 	check_result "b" 3
-	kill $NCPID >/dev/null 2>&1
+	qkill $NCPID 
+	if [ $RES -eq 1 ]
+	then
+		echo -n "."
+	else
+		break
+	fi
+	
 done
+print_result
 
+# Test 4
+rm -f 4.out
+nc -lp 3001 > 4.out & >/dev/null 2>&1
+NC1PID=$!
+sleep 1
+run_test 4 "TCP -> TCP" b
+sleep 1
+nc 127.0.0.1 3000 < a.in & >/dev/null 2>&1
+NC2PID=$!
+sleep 1
+qkill $NC2PID
+sleep 1
+check_result "a" 4
+print_result
+qkill $NC1PID
+qkill $NSPID
 
+# Test 5
 rm -f 5.out
 nc -lup 3001 > 5.out & >/dev/null 2>&1
 NC1PID=$!
 sleep 1
-run_test 5 "UDP -> UDP" & >/dev/null 2>&1
+run_test 5 "UDP -> UDP" b
 sleep 1
 nc -u 127.0.0.1 3000 < a.in & >/dev/null 2>&1
 NC2PID=$!
 sleep 1
 check_result "a" 5
-kill $NC2PID >/dev/null 2>&1
-kill $NC1PID >/dev/null 2>&1
-kill $NSPID >/dev/null 2>&1
+print_result
+qkill $NC2PID 
+qkill $NC1PID
+qkill $NSPID
 
-rm -f 4.out
-nc -lp 3001 > 4.out & >/dev/null 2>&1
-NC1PID=$!
-sleep 1
-run_test 4 "TCP -> TCP" & >/dev/null 2>&1
-sleep 1
-nc 127.0.0.1 3000 < a.in & >/dev/null 2>&1
-NC2PID=$!
-sleep 1
-kill $NC2PID >/dev/null 2>&1
-sleep 1
-check_result "a" 4
-kill $NC1PID >/dev/null 2>&1
-kill $NSPID >/dev/null 2>&1
-
+# Test 6
 rm -f 6.*.out
 run_test 6 "file -> more files"
+print_result q
+SUM=0
 check_result "a" "6.1"
+SUM=$(($SUM+$RES))
 check_result "a" "6.2"
+SUM=$(($SUM+$RES))
 check_result "a" "6.3"
+SUM=$(($SUM+$RES))
 check_result "a" "6.4"
+SUM=$(($SUM+$RES))
 check_result "a" "6.5"
+SUM=$(($SUM+$RES))
+RES=$SUM
+print_result
 
+# Test 7
 rm -f 7.out
-run_test 7 "Retry test" & >/dev/null 2>&1
+run_test 7 "Retry test" b
 nc -l -p 3001 > 7.out & >/dev/null 2>&1
 NC1PID=$!
 sleep 5
-kill $NC1PID >/dev/null 2>&1
+qkill $NC1PID
 if [ -s 7.out ]
 then 
 	echo -n "Retry..."
 	rm 7.out
 	nc -l -p 3001 > 7.out & >/dev/null 2>&1
 	NC1PID=$!
-	sleep 1
-	kill $NC1PID >/dev/null 2>&1
+	sleep 3
+	qkill $NC1PID
 	if [ -s 7.out ]
 	then
 		echo "OK"
+	else
+		echo "Failed"
+		FAIL=1
 	fi
 
 else
 	echo "Failed"
+	FAIL=1
 fi
-kill $NSPID >/dev/null 2>&1
+qkill $NSPID
 
+# Test 8 - invalid config file
+run_test 8 "Wrong config test"
+if [ $RES -eq 1 ]
+then
+	echo "OK"
+else
+	echo "Failed"
+	FAIL=1
+fi
+
+if [ $FAIL -eq 0 ]
+then
+	echo "All tests successfully passed"
+	exit 0;
+else
+	echo "Some tests failed"
+	exit 1;
+fi
